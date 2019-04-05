c3403a5b57c35d8197362f7797bd6028bbe56e24
sgadot <sebastien.gadot@lisi-automotive.com>
2019-04-05 08:50:55 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'My Store';
+
+
+  upperCase() {
+    this.title = this.title.toUpperCase()
+  }
+
+  lowercase() {
+    this.title = this.title.toLowerCase()
+  }
+}
